0feb5bb315beea2999d4206c1ed52683591fb72c
erik-a-bensen <bensen.erik.a@gmail.com>
2025-03-21 14:56:22 -0400

    added platform support

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -1,7 +1,8 @@
 from mpi4py import MPI
+import platform
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print(f"Hello, World! I am process {rank} of {size}.")
+print(f"Hello, World! I am process {rank} of {size}. Running on {platform.node()}")
