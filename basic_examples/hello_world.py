a3e1e0e37ea494d673a6c43a3f83bfdb9966bd16
erik-a-bensen <bensen.erik.a@gmail.com>
2025-04-06 19:27:03 -0400

    reorganized

--- basic_examples/hello_world.py
+++ basic_examples/hello_world.py
@@ -1,8 +1,18 @@
 from mpi4py import MPI
 import platform
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print(f"Hello, World! I am process {rank} of {size}. Running on {platform.node()}")
+print(f"Hello, World! I am process {rank} of {size}. Running on {platform.node()}") 
+
+def load_data_file():
+    None
+
+np.random.seed(rank)
+my_data = np.random.randn()
+avg = comm.reduce(my_data, op=MPI.SUM, root=0) / size 
+
+
